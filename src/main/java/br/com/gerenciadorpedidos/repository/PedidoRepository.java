8ddfa07d804cb281497322394c3482187a9711b8
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2026-03-02 02:35:17 -0300

    feat: elaborando derived querys para os repository produto e pedido

--- src/main/java/br/com/gerenciadorpedidos/repository/PedidoRepository.java
+++ src/main/java/br/com/gerenciadorpedidos/repository/PedidoRepository.java
@@ -3,5 +3,20 @@ package br.com.gerenciadorpedidos.repository;
 import br.com.gerenciadorpedidos.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByDataEngregaIsNull();
+
+    List<Pedido> findByDataEngregaIsNotNull();
+
+    List<Pedido> findByDataPedidoAfter(LocalDate data);
+
+    List<Pedido> findByDataPedidoBefore(LocalDate data);
+
+    List<Pedido> findByDataPedidoBetween(LocalDate dataInicio, LocalDate dataFim);
+
+
 }
--- src/main/java/br/com/gerenciadorpedidos/repository/ProdutoRepository.java
+++ src/main/java/br/com/gerenciadorpedidos/repository/ProdutoRepository.java
@@ -3,5 +3,32 @@ package br.com.gerenciadorpedidos.repository;
 import br.com.gerenciadorpedidos.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByNome(String nome);
+
+    List<Produto> findByCategoriaNome(String nomeCategoria);
+
+    List<Produto> findByPrecoGreaterThan(Double preco);
+
+    List<Produto> findByPrecoLessThan(Double preco);
+
+    List<Produto> findByNomeContaining(String termo);
+
+    List<Produto> findByNomeOrderByPrecoAsc(String nomeCategoria);
+
+    List<Produto> findByNomeContainingOrderByPrecoDesc(String nomeCategoria);
+
+    long countByNomeCategoria(String nomeCategoria);
+
+    long countByPrecoGreaterThan(Double preco);
+
+    List<Produto> findByPrecoLessThanOrNomeContaining(Double preco, String termo);
+
+    List<Produto> findByTop3ByPrecoDesc();
+
+    List<Produto> findByTop5ByCategoriaNomeOrderByPrecoAsc(String nomeCategoria);
+
 }
